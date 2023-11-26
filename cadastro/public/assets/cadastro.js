b00810c65dd180ef941a38aa4abf9f99d059e9cd
CarlosAtilaOliveira <carlosatilaoliveira@gmail.com>
2023-11-26 10:53:47 -0300

    criando usuario e validando campos

--- cadastro/public/assets/cadastro.js
+++ cadastro/public/assets/cadastro.js
@@ -1,5 +1,36 @@
-function logar() {
-    window.location.assign("/../login/public/login.html");
-}
 var botao = document.querySelector('.botao2');
 botao.addEventListener("click", logar);
+function logar() {
+    window.location.href = "/../login/public/login.html";
+}
+var botaoCadastro = document.querySelector('.botao');
+botaoCadastro.addEventListener('click', cadastro);
+var grupoUsuarios = [];
+function cadastro() {
+    var nomeInput = document.querySelector('#name');
+    var cpfInput = document.querySelector('#cpf');
+    var emailInput = document.querySelector('#email');
+    var senhaInput = document.querySelector('#senha');
+    var nome = nomeInput.value;
+    var cpf = cpfInput.value;
+    var email = emailInput.value;
+    var senha = senhaInput.value;
+    if ((nome.length < 4 || cpf.length !== 11)) {
+        alert("Complete seus Dados Corretamente");
+    }
+    else if ((nome.trim() !== '' && cpf.trim() !== ''
+        && email.trim() !== ''
+        && senha.trim() !== '')) {
+        var usuario = { nome: nome, cpf: cpf, email: email, senha: senha };
+        grupoUsuarios.push(usuario);
+        console.log(grupoUsuarios);
+        alert("Usuário Cadastrado Com Sucesso!!!");
+    }
+    else {
+        alert("Verifique todos os campos");
+    }
+    nomeInput.value = "";
+    cpfInput.value = "";
+    emailInput.value = "";
+    senhaInput.value = "";
+}
--- src/cadastro.ts
+++ src/cadastro.ts
@@ -1,6 +1,47 @@
-function logar(){
+let botao = document.querySelector('.botao2') as HTMLButtonElement;
+botao.addEventListener("click", logar);
+
+function logar() {
     window.location.href = "/../login/public/login.html";
 }
 
-let botao = document.querySelector('.botao2') as HTMLButtonElement;
-botao.addEventListener("click", logar);
+
+let botaoCadastro = document.querySelector('.botao') as HTMLButtonElement;
+botaoCadastro.addEventListener('click', cadastro);
+
+let grupoUsuarios: { nome: string, cpf: string, email: string, senha: string }[] = [];
+
+function cadastro() {
+    let nomeInput = (document.querySelector('#name') as HTMLInputElement);
+    let cpfInput = (document.querySelector('#cpf') as HTMLInputElement);
+    let emailInput = (document.querySelector('#email') as HTMLInputElement);
+    let senhaInput = (document.querySelector('#senha') as HTMLInputElement);
+
+    let nome = nomeInput.value;
+    let cpf = cpfInput.value;
+    let email = emailInput.value;
+    let senha = senhaInput.value;
+
+    if ((nome.length < 4 || cpf.length !== 11)) {
+
+        alert("Complete seus Dados Corretamente");
+
+    } 
+    else if ((nome.trim() !== '' && cpf.trim() !== '' 
+    && email.trim() !== '' 
+    && senha.trim() !== '')) {
+
+        let usuario = { nome, cpf, email, senha }
+        grupoUsuarios.push(usuario);
+        console.log(grupoUsuarios);
+        alert("Usuário Cadastrado Com Sucesso!!!");
+
+    } else {
+        alert("Verifique todos os campos");
+
+    }
+    nomeInput.value = "";
+    cpfInput.value = "";
+    emailInput.value = "";
+    senhaInput.value = "";
+}
